4c16aff1a1e9d9054ef5be6d142dd9c023173759
Anna-R-a <ratnikova_a.m@mail.ru>
2023-01-08 16:07:41 +0300

    feat: add code in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 module.exports = function toReadable (number) {
-  
+   	const upToTwenty = [
+		'',
+		'one',
+		'two',
+		'three',
+		'four',
+		'five',
+		'six',
+		'seven',
+		'eight',
+		'nine',
+		'ten',
+		'eleven',
+		'twelve',
+		'thirteen',
+		'fourteen',
+		'fifteen',
+		'sixteen',
+		'seventeen',
+		'eighteen',
+		'nineteen',
+	];
+	const tens = [
+		'',
+		'ten',
+		'twenty',
+		'thirty',
+		'forty',
+		'fifty',
+		'sixty',
+		'seventy',
+		'eighty',
+		'ninety',
+	];
+
+	if (number == 0){
+		return 'zero'
+	}
+	else if (number < 20){
+		return upToTwenty[number]
+	}
+	else if (number < 100 && number % 10 ==0){
+		return tens[Math.floor(number / 10)]
+	}
+	else if (number < 100 ){
+		return tens[Math.floor(number / 10)] + ' ' + upToTwenty[number % 10]
+	}
+	else if(number % 100 == 0){
+		return upToTwenty[Math.floor(number / 100)] + ' hundred'
+	}
+	else if(number % 10 == 0){
+		return upToTwenty[Math.floor(number / 100)] + ' hundred' + ' ' + tens[Math.floor(number % 100 / 10)]
+	}
+	else if(number % 100 >= 20){
+		return upToTwenty[Math.floor(number / 100)] + ' hundred' + ' ' + tens[Math.floor(number % 100 / 10)] + ' ' + upToTwenty[number % 10]
+	}
+		else {
+		return upToTwenty[Math.floor(number / 100)] + ' hundred' + ' ' + upToTwenty[number % 100];
+	}
 }
